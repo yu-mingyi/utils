412ef3cabd7846f1eed8a2abf69e6ccfa31f13fb
mingyi_yu <yu.mingyi@gmail.com>
2024-09-01 23:35:18 +0800

    add pytemplate

---
+++ pytemplate.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+####################
+# Required Modules #
+####################
+
+# Generic/Built-in
+
+
+# Libs
+
+
+# Custom
+
+
+##################
+# Configurations #
+##################
+
+
+#############
+# Functions #
+#############
+
+
+###########
+# Classes #
+###########
+
+
+##########
+# Script #
+##########
+
+
+if __name__ == '__main__':
+    pass
